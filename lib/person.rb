f901ac9f822a8640c4081e165b7b3f9797521721
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-10 02:11:41 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(atributes)
-    
+  def initialize(attributes)
+    attributes.each {|k, v| ("#{k}=v") k.send
     
   end 
 end
